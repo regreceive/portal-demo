7803b08e596590c5ddd8222ccb9d610e49e1d3e3
liyao <liyao@k2data.com.cn>
2021-12-06 17:17:22 +0800

    提取gql

--- packages/entry/config/portal.ts
+++ packages/entry/config/portal.ts
@@ -4,13 +4,15 @@ const portal: IConfigFromPlugins['portal'] = {
   /** appKey是应用在建模器配置的应用key */
   appKey: 'test',
   /** nacos 如果打开注释，会自动请求线上配置，其配置会覆盖与本地service合并 */
-  // nacos: '/nacos/v1/cs/configs?dataId=dfem.front.portal&group=default',
+  nacos: {
+    // url: '/nacos/v1/cs/configs?dataId=dfem.front.portal&group=default',
+  },
   appDefaultProps: {
     appKey: 'app1',
   },
-  mainApp: {
-    appPath: '/web/demo/apps'
-  }
+  // mainApp: {
+  //   appPath: '/web/demo/apps',
+  // },
 };
 
 export default portal;
--- packages/features/src/pages/Home/index.tsx
+++ packages/features/src/pages/Home/index.tsx
@@ -2,21 +2,15 @@ import { FC, useCallback, useState } from 'react';
 import { Button, Typography } from 'antd';
 import { api } from 'k2-portal';
 import BoxArea from '@/components/BoxArea';
+import queryMenu from './menu.gql';
 
 const Home: FC = () => {
   const [result, setResult] = useState<any>({});
 
   // 获得菜单
   const handleBtnClick = useCallback(async () => {
-    const myQuery = `#graphql
-      {
-        bcf_front_menu {
-          title
-          app_key
-        }
-      }
-    `;
-    const res = await api.graphql.post({ query: myQuery });
+    const res = await api.graphql.post(queryMenu);
+    setResult(res);
   }, []);
 
   return (
@@ -25,7 +19,7 @@ const Home: FC = () => {
         <Button onClick={handleBtnClick}>获得菜单</Button>
         <Typography.Paragraph>
           <pre style={{ maxHeight: 200, overflow: 'hidden auto' }}>
-            Response: {JSON.stringify(result, null, 4)}
+            Response: {JSON.stringify(result, null, 2)}
           </pre>
         </Typography.Paragraph>
       </BoxArea>
