fc912bb55c50a90d7bab3dba1945467e25ded2e2
liyao <regreceive@163.com>
2022-05-01 00:19:13 +0800

    部署

---
+++ deploy.js
@@ -0,0 +1,18 @@
+var process = require("child_process");
+let path = require("path");
+
+try {
+  process.execSync("rm -rf dist", { cwd: path.join(__dirname) });
+} catch {}
+
+process.exec("mkdir -p dist/");
+process.execSync("yarn workspace portal build");
+process.execSync("cp -r ./packages/portal/dist/ ./dist/");
+
+process.exec("mkdir -p dist/apps/entry");
+process.execSync("yarn workspace entry build");
+process.execSync("cp -r ./packages/entry/dist/ ./dist/apps/entry/");
+
+process.exec("mkdir -p dist/apps/antd-ui");
+process.execSync("yarn workspace antd-ui build");
+process.execSync("cp -r ./packages/antd-ui/dist/ ./dist/apps/antd-ui/");
--- packages/portal/src/pages/index.tsx
+++ packages/portal/src/pages/index.tsx
@@ -1,19 +1,9 @@
-import { FC, useMemo, useEffect } from 'react';
-import { portal, Widget, useLazyQuery } from 'k2-portal';
+import { FC, useMemo } from 'react';
+import { portal, Widget } from 'k2-portal';
 import { useLocation } from 'umi';
-import { api } from 'k2-portal';
-import service from './index.gql';
 
 const Portal: FC = () => {
   const location = useLocation();
-  const [getApps, { data }] = useLazyQuery(service.apps.gql);
-
-  useEffect(() => {
-    console.log('hello');
-    
-    getApps();
-  }, []);
-
   const [appKey, layout] = useMemo(() => {
     return [portal.currAppKey, portal.currLayout];
   }, [location.pathname]);
