3f7af83214484c36becabed9023a3e74b8a5a4f1
liyao <regreceive@163.com>
2022-05-01 01:36:55 +0800

    添加说明

--- README.md
+++ README.md
@@ -1,21 +1,5 @@
-# portal 应用合集
+# k2-portal-demo
 
-使用`k2-portal`开发框架制作微前端 demo，展示开发框架的特性，以及用作开发指南。
+此项目演示如何使用 k2-portal 微前端框架创建应用和子应用。
 
-## 安装
-
-该项目基于 lerna，指定`yarn`作为包管理工具，请先确保本地已安装 node.js 和 yarn
-
-```shell
-yarn install
-```
-
-## 运行
-
-使用 vscode 会提高开发体验，可以轻松运行指定模块，如果选择命令行运行启动的话，请执行：
-
-```shell
-yarn workspace xxx run start
-```
-
-> xxx 代表 packages/下的模块名称。
+[Demo](https://k2-portal-demo.vercel.app/)
--- deploy.js
+++ deploy.js
@@ -16,3 +16,15 @@ process.execSync("cp -r ./packages/entry/dist/* ./dist/apps/entry/");
 process.exec("mkdir -p dist/apps/antd-ui");
 process.execSync("yarn workspace antd-ui build");
 process.execSync("cp -r ./packages/antd-ui/dist/* ./dist/apps/antd-ui/");
+
+process.exec("mkdir -p dist/apps/compatibility");
+process.execSync("yarn workspace compatibility build");
+process.execSync("cp -r ./packages/compatibility/dist/* ./dist/apps/compatibility/");
+
+process.exec("mkdir -p dist/apps/features");
+process.execSync("yarn workspace features build");
+process.execSync("cp -r ./packages/features/dist/* ./dist/apps/features/");
+
+process.exec("mkdir -p dist/apps/editor");
+process.execSync("yarn workspace editor build");
+process.execSync("cp -r ./packages/editor/dist/* ./dist/apps/editor/");
