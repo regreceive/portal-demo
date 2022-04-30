ed1854637fe644889098789d4fa722242294e9fd
liyao <regreceive@163.com>
2022-05-01 01:21:28 +0800

    fix folder along with 'dist'

--- deploy.js
+++ deploy.js
@@ -7,12 +7,12 @@ try {
 
 process.exec("mkdir -p dist/");
 process.execSync("yarn workspace portal build");
-process.execSync("cp -r ./packages/portal/dist/ ./dist/");
+process.execSync("cp -r ./packages/portal/dist/* ./dist/");
 
 process.exec("mkdir -p dist/apps/entry");
 process.execSync("yarn workspace entry build");
-process.execSync("cp -r ./packages/entry/dist/ ./dist/apps/entry/");
+process.execSync("cp -r ./packages/entry/dist/* ./dist/apps/entry/");
 
 process.exec("mkdir -p dist/apps/antd-ui");
 process.execSync("yarn workspace antd-ui build");
-process.execSync("cp -r ./packages/antd-ui/dist/ ./dist/apps/antd-ui/");
+process.execSync("cp -r ./packages/antd-ui/dist/* ./dist/apps/antd-ui/");
