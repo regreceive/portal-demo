a94ed968855e15c9339a47e2fcb0eab7a23aab10
regreceive <regreceive@163.com>
2022-05-07 11:16:13 +0800

    delete package

--- packages/baidu-map/src/app.tsx
+++
@@ -1,9 +0,0 @@
-// lightTheme是明亮风格，darkTheme是暗色调风格
-
-export const lightTheme = {
-  '--basicLayout-bgColor': '#eee',
-};
-
-export const darkTheme = {
-  '--basicLayout-bgColor': '#333',
-};
--- packages/baidu-map/src/layouts/BasicLayout.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './BasicLayout.less';
-
-const BasicLayout: React.FC = (props) => {
-  return <div className={styles.container}>{props.children}</div>;
-};
-
-export default BasicLayout;
